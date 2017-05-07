318c38afa4dc3a27b61e6c5c185edfe9a7c488ce
nhannguyen1994 <nhank57tha@gmail.com>
2017-05-07 18:45:46 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-
 function drawManyDiamond(N, H) {
-    if (N < 1 || H < 1 || H % 2 == 0) {
+    if (N <= 1 || H <= 1 || H % 2 == 0) {
         console.log("N nhỏ hơn 1 hoặc H nhỏ hơn 1 hoặc H là số chẵn, nhập lại N va H ")
     }
-   let h = Math.floor((H / 2) + 1); // H=7, h=4
-   let width = H - 1; // width=6
-
+    let h = Math.floor((H / 2) + 1); // H=7, h=4
+    let width = H - 1; // width=6
     for (let i = 0; i < H; i++) {
+        let str = '';
         if (i < h) {
-            for (let j = 0; j < N * H - 2; j++) {
+            for (let j = 0; j < N * H - N+1; j++) {
                 let j2 = j % width;
                 if ((j2 === h - 1 - i) || (j2 === h - 1 + i)) {
-                    console.log("*");
+                    str += "*";
                 } else {
-                    console.log(" ");
+                    str += ' ';
                 }
             }
         }
-        else {
-            for (let j = 0; j < N * H - 2; j++) {
-                let j2 = j % width;
-                if ((j2 === i - h + 1) || (j2 === h - ((i - h + 1)))) {
-                    console.log("*");
-                } else {
-                    console.log(" ");
-                }
-            }
 
+        else {
+            for (let j = 0; j < N * H - N+1; j++) {
+                let j2 = j % width;
+                if ((j2 === i - h + 1) || (j2 === width- ((i - h )+ 1))) {
+                    str += '*';
+                }
+                else {
+                    str += ' ';
+                }
+            }
         }
+
+        console.log(str);
+
     }
+
 }
 drawManyDiamond(3,7);
